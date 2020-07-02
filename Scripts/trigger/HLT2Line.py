02a20e9736e6b78fc9230e29a5b99557e3840033
Jan Rol <janrol@hotmail.com>
2020-07-02 14:30:10 +0200

    Added example and prototype HLT2Line

--- Scripts/trigger/HLT2Line.py
+++ Scripts/trigger/HLT2Line.py
@@ -1,25 +1,26 @@
-# First structure for HLT(2)Line.
+# First structure for HLT2Line.
 
 # Jan Rol 01-07-2020
 
 # Triggers on B+ -> tau - > pi+ pi- pi+
 
 from Moore.config import HltLine, register_line_builder
-from ..standard.particles import make_has_rich_long_pions
+
+from GaudiKernel.SystemOfUnits import Mev, GeV
 
 from RecoConf.reco_objects_from_file import upfront_reconstruction, make_pvs
 
-from GaudiKernel.SystemOfUnits import Mev, GeV
 from ..algorithms import (
     require_all,
     ParticleCombinerWithPVs,
-    ParticleFilterWithPVs
-)
+    ParticleFilterWithPVs)
+from ..framework import configurable
+from ..standard.particles import make_has_rich_long_pions
 
 
 all_lines = {}
 
-#@configurable ?
+@configurable
 def filter_particles(particles, pvs, pt_min=0.5*GeV, pid=None):
 #Prototype of particle maker. Takes input pid and cuts.    
     code = require_all(
@@ -30,12 +31,12 @@ def filter_particles(particles, pvs, pt_min=0.5*GeV, pid=None):
         code += " & ({0})".format(pid)
     return ParticleFilterWithPVs(particles, pvs, Code=code)
 
-#@configurable ?
-def make_pions(particles, pvs, pid):
-    return filter_particles(particles, pvs, pid=pid) # Pion ID?
+@configurable
+def make_pions(particles, pvs, pid="PIDK < 5"):
+    return filter_particles(particles, pvs, pid=pid)
 
-#@configurable ?
-def make_taus(particles=pions, pvs): # Add cuts as fucntion arguments
+@configurable
+def make_taus(particles=pions, pvs): # Add cuts as function arguments
     combination_code = require_all(
         "")
     vertex_cut = require all(
@@ -43,12 +44,12 @@ def make_taus(particles=pions, pvs): # Add cuts as fucntion arguments
     return ParticleCombinerWithPVs(
         particles=particles,
         pvs=pvs,
-        DecayDiscriptor="tau+ -> pi+ pi- pi+",
+        DecayDiscriptor="[tau+ -> pi+ pi- pi+]cc",
         CombinationCut=combination_code,
         MotherCut=vertex_cut
     )
 
-#@configurable ?
+@configurable
 def make_bus(particles=taus, pvs):   # Add cuts as function arguments
     combination_code = require_all(
         "")
@@ -57,19 +58,18 @@ def make_bus(particles=taus, pvs):   # Add cuts as function arguments
     return ParticleCombinerWithPVs(
         particles=particles,
         pvs=pvs,
-        DecayDiscriptor="B+ -> tau+",
+        DecayDiscriptor="[B+ -> tau+]cc",
         CombinationCut=combination_code,
         MotherCut=vertex_cut
     )
 
-
 @register_line_builder(all_lines)
-#@configurable ?
+@configurable
 def butotautopipipi(name = "BuToTauToPipPimPip", prescale = 1):
     pvs = make_pvs()
     pions = make_pions(make_has_rich_long_pions(), pvs)
-    taus = make_taus(particles=pions, descriptors=["tau+ -> pi+ pi- pi+"], pvs=pvs)
-    bus = make_bus(particles=taus, descriptors=["B+ -> tau+"], pvs=pvs)
+    taus = make_taus(particles=pions, pvs=pvs)
+    bus = make_bus(particles=taus, pvs=pvs)
 return HltLine(
     name=name,
     algs=upfront_reconstruction() + [bus],
---
+++ Scripts/trigger/hlt1_example.py
@@ -0,0 +1,6 @@
+from Moore import options, run_moore
+
+def make_lines():
+    return []
+
+run_moore(options, make_lines)
